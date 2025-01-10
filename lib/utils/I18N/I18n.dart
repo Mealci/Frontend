e9f770a24021e717615eaf0407f7d507071e5cde
lucascasado <lucas.casado@epitech.eu>
2025-01-10 10:37:17 +0100

    format

--- lib/utils/I18N/I18n.dart
+++ lib/utils/I18N/I18n.dart
@@ -1,7 +1,5 @@
 class I18n {
-
   static String? getTranslation(Map<dynamic, String> map, dynamic key) {
     return map[key];
   }
-
 }
--- lib/utils/env/environnementvariable.dart
+++ lib/utils/env/environnementvariable.dart
@@ -1,10 +1,7 @@
-
 class EnvironnementVariable {
-
   static String apiUrl = '';
 
   static const String apiUrlDev = 'http://localhost:8080/api';
 
   static const String apiUrlProd = 'https://prod.api.com';
-
 }
