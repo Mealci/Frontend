a24f45032125cc7722927f349f45b180099f1cdf
InesGarcia84 <ines.garcia@epitech.eu>
2025-03-21 17:27:07 +0100

    feat(service): Add deleteFoodById and patchFoodQuantityById methods

--- lib/services/frigo_service.dart
+++ lib/services/frigo_service.dart
@@ -53,6 +53,92 @@ class FrigoService {
           'Échec du chargement des données des catégories d\'aliments');
     }
   }
+
+  void deleteFoodById(BuildContext context, int id) async {
+    String? token = await _storage.readData('token_jwt');
+
+    if (token == null) {
+      _logger.info('Utilisateur non authentifié. Veuillez vous connecter.');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content:
+                Text('Utilisateur non authentifié. Veuillez vous connecter.')),
+      );
+      Navigator.pushNamed(context, '/loginThirdPage');
+    }
+
+    final String deleteFoodPath = '/food/deleteFood?id=$id';
+    final Uri deleteFoodUri =
+        Uri.parse('${EnvironnementVariable.apiUrl}$deleteFoodPath');
+
+    final response = await http.delete(deleteFoodUri, headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $token',
+    });
+
+    if (response.statusCode == 401) {
+      _logger.info('Session expirée. Veuillez vous reconnecter.');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text('Session expirée. Veuillez vous reconnecter.')),
+      );
+      Navigator.pushNamed(context, '/loginThirdPage');
+    }
+
+    if (response.statusCode == 200) {
+      _logger.info('Aliment supprimé avec succès');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Aliment supprimé avec succès')),
+      );
+    } else {
+      throw Exception('Échec de la suppression de l\'aliment');
+    }
+  }
+
+  Future patchFoodQuantityById(
+      BuildContext context, int id, double quantity) async {
+    String? token = await _storage.readData('token_jwt');
+
+    if (token == null) {
+      _logger.info('Utilisateur non authentifié. Veuillez vous connecter.');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content:
+                Text('Utilisateur non authentifié. Veuillez vous connecter.')),
+      );
+      Navigator.pushNamed(context, '/loginThirdPage');
+    }
+
+    final String patchFoodPath = '/food/quantity/$id/$quantity';
+    final Uri patchFoodUri =
+        Uri.parse('${EnvironnementVariable.apiUrl}$patchFoodPath');
+
+    final response = await http.patch(patchFoodUri,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer $token',
+        },
+        body: jsonEncode({'id': id, 'quantity': quantity}));
+
+    if (response.statusCode == 401) {
+      _logger.info('Session expirée. Veuillez vous reconnecter.');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text('Session expirée. Veuillez vous reconnecter.')),
+      );
+      Navigator.pushNamed(context, '/loginThirdPage');
+    }
+
+    if (response.statusCode == 200) {
+      _logger.info('Quantité de l\'aliment modifiée avec succès');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text('Quantité de l\'aliment modifiée avec succès')),
+      );
+    } else {
+      throw Exception('Échec de la modification de la quantité de l\'aliment');
+    }
+  }
 }
 
 List<Food> parseFoodByCategory(String responseBody) {
