696e93d09f8e90b662bacd42ef2fcedcc8f4a6b8
Lucas Casado <lucas.casado@epitech.eu>
2025-03-24 15:31:15 +0100

    fix nav bar

--- lib/components/navbar.dart
+++ lib/components/navbar.dart
@@ -16,7 +16,7 @@ class NavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     // Récupérer les dimensions de l'écran
     final screenHeight = MediaQuery.of(context).size.height;
-    final textScale = MediaQuery.of(context).textScaleFactor;
+    final textScale = MediaQuery.textScaleFactorOf(context);
 
     return Container(
       height: screenHeight * 0.1,
