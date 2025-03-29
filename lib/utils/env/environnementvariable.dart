981a93c704320de82cdf332727eaa7f98062b2e0
InesGarcia84 <ines.garcia@epitech.eu>
2025-03-29 12:07:50 +0100

    fix(): Refactor .gitignore and update API URL handling

--- lib/utils/env/environnementvariable.dart
+++ lib/utils/env/environnementvariable.dart
@@ -3,5 +3,6 @@ class EnvironnementVariable {
 
   static const String apiUrlDev = 'http://localhost:8080';
 
-  static const String apiUrlProd = 'https://prod.api.com';
+  static const String apiUrlProd =
+      'https://app-630e87c8-37d2-4a77-ba65-1b078029563c.cleverapps.io';
 }
